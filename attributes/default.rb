58566ee9fa137280c56251ee747b7194daf71c76
Christopher Papke <christopher@silkstart.com>
2017-05-25 09:01:24 -0700

    revert to 0.12.2.1

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,7 +4,7 @@ default['wkhtmltopdf']['install_dir'] = '/usr/local/bin'
 default['wkhtmltopdf']['lib_dir']     = ''
 
 default['wkhtmltopdf-update']['major_version'] = '0.12'
-default['wkhtmltopdf-update']['version'] = '0.12.4'
+default['wkhtmltopdf-update']['version'] = '0.12.2.1'
 
 case node['platform_family']
 when 'mac_os_x', 'mac_os_x_server'
